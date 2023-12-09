9dfef64c588c433585e492fa51695c416cbd061a
Mohamed-Yousry-Ali <mohamedyousry631@gmail.com>
2023-12-09 16:24:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import Dashboard from './components/Dashboard';
+import Notfound from './components/Notfound';
+
+
+const routes = createBrowserRouter([{
+  path: "", element: <Layout />, children: [
+    {index:true,element:<Dashboard/>},
+  
+    { path: "*", element: <Notfound /> }
+  ]
+}])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={routes}></RouterProvider>
+    </>
   );
 }
 
